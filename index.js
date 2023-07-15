40b462709ae19c6076164f7ae35bb4170edb2477
Utkarsh <utkarshmishra101394@gmail.com>
2023-07-15 15:26:22 +0530

    commiting change

---
+++ index.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+const homeCSS = fs.readFileSync("home.css");
+
+const replaceVal = (tempVal, orgVal, cssFile) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+  temperature = temperature.replace("/* {%home.css%} */", cssFile);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "https://api.openweathermap.org/data/2.5/weather?q=Agra&appid=9355606bda39077f34b9ed91f6447e88"
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        // console.log(arrData[0].main.temp);
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val, homeCSS))
+          .join("");
+        res.write(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  }
+});
+
+server.listen(8000, "127.0.0.1");
